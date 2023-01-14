6e7e8176b38b29d418b7b1bfdfd8f719e96f936e
root <root@kali.azatezalo>
2023-01-14 17:21:47 -0500

    Gradel bugs solved.

--- app/src/main/java/com/example/notepad/Database/RoomDB.java
+++ app/src/main/java/com/example/notepad/Database/RoomDB.java
@@ -1,9 +1,12 @@
 package com.example.notepad.Database;
 
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
+
 import android.content.Context;
+
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 import com.example.notepad.Models.Notes;
 
